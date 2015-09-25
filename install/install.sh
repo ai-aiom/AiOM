44b206a7951b72e9047535323ef1087504eed496
zhangli <zhangli11@asiainfo.com>
2015-09-25 10:47:07 +0800

    安装脚本更新

--- install/install.sh
+++ install/install.sh
@@ -9,7 +9,7 @@ fi
 sed -n '1,/^exit 0$/!p' $0 >$install_dir/aiom.tar.gz
 
 cd $install_dir
-tar -xzvf auth.tar.gz
+tar -xzvf aiom.tar.gz
 rm -rf /usr/local/lib/tomcat/webapps/aiom*
 cp aiom.war /usr/local/lib/tomcat/webapps
 
@@ -19,11 +19,20 @@ else
     mkdir /etc/aiom
 fi
 
-cd $install_dir
 cp conf/* /etc/aiom
 cp init.d/aiom /etc/rc.d/init.d
 
 chmod 755 /etc/rc.d/init.d/aiom
 chkconfig aiom on
+
+path=$(pwd)
+mysql -uroot << EOF
+source $path/sql/create_database.sql;
+use aiom
+source $path/sql/create_objects.sql;
+source $path/sql/init_data.sql;
+EOF
+
+rm -rf $install_dir
 
 exit 0
