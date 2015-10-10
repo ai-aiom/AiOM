f9714718f6833f95ea826bf5f539673faf0446ef
鲁洋 <luyang@asiainfo.com>
2015-10-10 10:46:51 +0800

    1、机柜删除时，如果包含服务器则不允许删除。

--- src/main/java/com/asiainfo/aiom/view/system/cabinet/CabinetDeleteAction.java
+++ src/main/java/com/asiainfo/aiom/view/system/cabinet/CabinetDeleteAction.java
@@ -9,7 +9,12 @@
  */
 package com.asiainfo.aiom.view.system.cabinet;
 
+import java.util.List;
+
+import com.asiainfo.gim.client.server_manage.api.ServerApi;
+import com.asiainfo.gim.client.server_manage.domain.Server;
 import com.asiainfo.gim.client.site.api.CabinetApi;
+import com.asiainfo.support.struts2.ResultBean;
 import com.asiainfo.support.struts2.ServletAwareActionSupport;
 
 /**
@@ -19,9 +24,13 @@ import com.asiainfo.support.struts2.ServletAwareActionSupport;
 public class CabinetDeleteAction extends ServletAwareActionSupport
 {
 	private static final long serialVersionUID = 7820845521544123443L;
-	
+
 	private CabinetApi cabinetApi;
-	
+
+	private ServerApi serverApi;
+
+	private ResultBean resultBean;
+
 	private Integer id;
 
 	public Integer getId()
@@ -38,10 +47,35 @@ public class CabinetDeleteAction extends ServletAwareActionSupport
 	{
 		this.cabinetApi = cabinetApi;
 	}
-	
+
+	public void setServerApi(ServerApi serverApi)
+	{
+		this.serverApi = serverApi;
+	}
+
+	public ResultBean getResultBean()
+	{
+		return resultBean;
+	}
+
+	public void setResultBean(ResultBean resultBean)
+	{
+		this.resultBean = resultBean;
+	}
+
 	public String execute()
 	{
+		List<Server> servers = serverApi.listServers();
+		for (Server server : servers)
+		{
+			if (server.getSite() != null && server.getSite().getRack() != null && server.getSite().getRack() == id)
+			{
+				resultBean = new ResultBean(false, "此机柜下存在服务器：" + server.getIp() + "，无法删除！");
+				return SUCCESS;
+			}
+		}
 		cabinetApi.deleteCabinet(id);
+		resultBean = new ResultBean(true, null);
 		return SUCCESS;
 	}
 }
